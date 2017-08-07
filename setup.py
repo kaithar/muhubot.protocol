4004ede1b2eddca54d58146e4a10d5dba8bb2a64
Kaithar <noreply.commiter@the-cell.co.uk>
2017-08-07 08:30:37 +0000

    Split from main repo

---
+++ setup.py
@@ -0,0 +1,37 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='muhubot.protocol',
+    version='0.1.0',
+    description='Client protocol for Muhubot\'s fabric',
+    long_description=long_description,
+    url='https://github.com/kaithar/muhubot.protocol',
+    author='Kaithar',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='muhubot',
+    packages=['muhubot.protocol'],
+    install_requires=['pyzmq']
+)
